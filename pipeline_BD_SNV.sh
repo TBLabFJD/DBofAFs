dbfc9a2bf6b4d708e0969209f7cff769b5d6cb36
Graciela Uria Regojo <73877383+guriaregojo@users.noreply.github.com>
2024-06-08 12:55:47 +0200

    faltan unas comillas

--- pipeline_BD_SNV.sh
+++ pipeline_BD_SNV.sh
@@ -243,18 +243,18 @@ done < "$dup_file"
 ###### ahora YA SE HAN QUEDADO  los duplicate samples renombrados tal que así: repeat119-0986.hg38.gatk.CES.v41.20240315.vcf.gz repeat219-0986.hg38.gatk.CES.v41.20240315.vcf.gz y así estan controlados y tambien sus correspondientes BEDs
 # tras dejarlos renombrados con repeat1XX-XXXX.vcf.gz, repeat2XX-XXX.vcf.gz hay que ABRIR los vcfs y 
 #renombrar todas las veces que aparezca el sample name XX-XXXX y cambairlo por repeatYXX-XXX
-for vcffile in ${path_maf}/individual_vcf/*/repeat*.gz 
-do
+for vcffile in ${path_maf}/individual_vcf/*/repeat*.gz; do
 	dir=$(dirname "${vcffile}")
-    	filename=$(basename "${vcffile}")
-        # Extract the pattern (YXX-XXX) from the filename
-    	pattern=$(echo "${filename}" | grep -oP 'repeat\K\d+-\d+')
-        # Define the replacement pattern
-    	replacement="repeat${pattern}"
-     	## cambiar todos los XX-XXXX por repeatYXX-XXX, comprimir vcf y guardar en temporary file
-     	bcftools view ${vcffile} | sed "s/XX-XXX/${replacement}/g" | bgzip -c > ${path_maf}/individual_vcf/tmp.vcf.gz"
+	filename=$(basename "${vcffile}")
+	# Extract the pattern (YXX-XXX) from the filename
+	pattern=$(echo "${filename}" | grep -oP 'repeat\K\d+-\d+')
+	# Define the replacement pattern
+	replacement="repeat${pattern}"
+	## cambiar todos los XX-XXXX por repeatYXX-XXX, comprimir vcf y guardar en temporary file
+	bcftools view ${vcffile} | sed "s/XX-XXX/${replacement}/g" | bgzip -c > "${path_maf}/individual_vcf/tmp.vcf.gz"
 	mv ${path_maf}/individual_vcf/tmp.vcf.gz ${vcffile}
 done
+ 
 
 
 ################### ESTE CODIGO ES SOLO PARA CUANDO YA HABIA INCORPORATED VCFS
