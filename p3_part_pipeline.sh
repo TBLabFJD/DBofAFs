34c7e12265b09495ec7f2fafd4565717688b270c
Graciela Uria Regojo <73877383+guriaregojo@users.noreply.github.com>
2024-07-11 20:39:33 +0200

    Update p3_part_pipeline.sh

--- p3_part_pipeline.sh
+++ p3_part_pipeline.sh
@@ -57,3 +57,61 @@ echo >> ${path_maf}/metadata/${date_dir}/logfile.txt
 echo "INICIO:"
 echo $(date)
 
+#=======#
+# Merge #
+#=======#
+
+echo "MERGE" >> ${path_maf}/metadata/${date_dir}/logfile.txt
+echo "MERGE"
+STARTTIME=$(date +%s)
+
+# BCFTOOLS da error si hay muchos vcfs. Para prevenir el error he puesto como máximo 500 vcfs para hacer vcfs intermedios.
+ls ${path_maf}/individual_vcf/new_vcf/*.vcf.gz ${path_maf}/individual_vcf/incorporated_vcf/*.vcf.gz | split -l 850 - "${path_maf}/tmp/subset_vcfs_"
+
+function MERGED {
+
+	path_maf=${1}
+	iname="$(basename ${2})"
+
+	bcftools merge -l ${2} -O z -o ${path_maf}/tmp/merge.${iname}.vcf.gz
+	tabix -p vcf ${path_maf}/tmp/merge.${iname}.vcf.gz
+
+} 
+
+export -f MERGED
+parallel "MERGED" ::: ${path_maf} ::: ${path_maf}/tmp/subset_vcfs_*
+
+#EDIT GUR: ESTA LINEA DE CODIGO ESTA JUNTANDO LOS VCFS QUE SE SEPARARON ANTES. GONZALO DECIA QUE SI HABIA MÁS DE 
+#500 VCFS HABIA QUE REPARTIRLOS EN TROZOS, O SEA: si hay 1500 vcfs en la carpeta new_vcf se HARIAN 3 MERGE: merge_aa, merge_bb, merge_cc y cada uno tendria 
+#500 VCFs y despues hay que juntar esos 3 VCFS de 500 vcfs cada uno en 1 merge (para tener los 1500)
+#PROBLEMA: si hay menos de 500 VCFs en el new_vcf solo hay 1 VCF: merge_aa, entonces esta linea de abajo da error porque no 
+#esta haciendo merge de varios VCFs, ya que solo hay 1. Y por eso da el error de que no puede hacer merge
+
+#linea original GONZALO
+#bcftools merge -O z -o ${path_maf}/tmp/merged_${date_paste}_tmp.vcf.gz ${path_maf}/tmp/merge.*.vcf.gz
+
+#GUR EDIT: cp el merge original (merge_aa) en uno nuevo que se llame como lo del date_paste
+#cp ${path_maf}/tmp/merge.*.vcf.gz ${path_maf}/tmp/merged_${date_paste}_tmp.vcf.gz
+
+
+# Count the number of VCFs (merge_aa, merge_bb...)
+file_count=$(ls -1 "${path_maf}/tmp/merge."*.vcf.gz 2>/dev/null | wc -l)
+if [ "$file_count" -gt 1 ]; then
+    # HAY MÁS DE 1 VCF PARA MERGE: merge_aa, merge_bb.. ORIGINALMENTE: >500 VCF rn la carpeta
+	echo LINEA GONZALO 
+	bcftools merge -O z -o ${path_maf}/tmp/merged_${date_paste}_tmp.vcf.gz ${path_maf}/tmp/merge.*.vcf.gz
+
+else
+    # solo hay 1 VCF (MERGE_AA), no hay que merge nada: originalmente <500 VCF en new_vcf
+	echo LINEA GRACIELA
+	cp ${path_maf}/tmp/merge.*.vcf.gz ${path_maf}/tmp/merged_${date_paste}_tmp.vcf.gz
+fi
+
+#comment gonzalo
+#######if [[ $(ls ${path_maf}/individual_vcf/new_vcf/*.vcf.gz | wc -l) -gt 850 ]]
+
+
+ENDTIME=$(date +%s)
+echo "Running time: $(($ENDTIME - $STARTTIME)) seconds" >> ${path_maf}/metadata/${date_dir}/logfile.txt
+echo >> ${path_maf}/metadata/${date_dir}/logfile.txt
+echo "Running time: $(($ENDTIME - $STARTTIME)) seconds"
